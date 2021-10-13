e23ae78f69185de0833a5fea939fbfc02726c79d
Alexander Trufkin <Alexander.Trufkin@Technosoft.md>
2021-10-13 11:23:34 +0300

    fixed not working assert by replacing find_element_by_css_selector with find_elements_by_css_selector

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_basket_button_presence(browser):
     browser.get(link)
     time.sleep(30)
-    assert browser.find_elements_by_css_selector("qbutton.btn-add-to-basket"), "'Add to basket' button is missing"
+    assert browser.find_elements_by_css_selector("button.btn-add-to-basket"), "'Add to basket' button is missing"
